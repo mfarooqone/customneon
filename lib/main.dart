3297dca990bb557a133b7d366fc1722528fc0ab8
Muhammad Farooq Iqbal <mfarooqiqbal143@gmail.com>
2024-03-11 16:34:15 +0500

    WIP production

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,3 @@
-import 'package:customneon/desktop_view/auth_view/signin_view.dart';
-import 'package:customneon/desktop_view/auth_view/signup_view.dart';
-import 'package:customneon/desktop_view/footer/payment_methods.dart';
-import 'package:customneon/dummy.dart';
 import 'package:customneon/homepage/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -18,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Sizer(
       builder: (context, orientation, deviceType) {
-        return  GetMaterialApp(
+        return const GetMaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Custom Neon',
           home: HomePage(),

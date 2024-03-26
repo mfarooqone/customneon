c14578140833ce3815860878d03be2fed7be69eb
Muhammad Farooq Iqbal <mfarooqiqbal143@gmail.com>
2024-03-27 03:19:37 +0500

    WIP refund policy

---
+++ lib/controllers/policies_controller.dart
@@ -0,0 +1,47 @@
+import 'package:customneon/network_client/network_client.dart';
+import 'package:customneon/utills/show_messages.dart';
+import 'package:get/get.dart';
+
+class PoliciesController extends GetxController {
+  RxBool isLoading = false.obs;
+  RxString privacyPolicy = ''.obs;
+  RxString refundPolicy = ''.obs;
+
+  ///
+
+  Future<void> getPrivacy() async {
+    isLoading.value = true;
+
+    final result = await Get.find<NetworkClient>().get(
+      "/policies/privacy-policy",
+      sendUserAuth: true,
+    );
+    if (result.isSuccess) {
+      privacyPolicy.value = result.rawData;
+      isLoading.value = false;
+    } else {
+      showErrorMessage(result.message!);
+      isLoading.value = false;
+    }
+  }
+
+  ///
+  ///
+  Future<void> getRefundPolicy() async {
+    isLoading.value = true;
+
+    final result = await Get.find<NetworkClient>().get(
+      "/policies/refund-policy",
+      sendUserAuth: true,
+    );
+    if (result.isSuccess) {
+      refundPolicy.value = result.rawData;
+      isLoading.value = false;
+    } else {
+      showErrorMessage(result.message!);
+      isLoading.value = false;
+    }
+  }
+
+  ///
+}

7492cf9c8e9c6df9ddf02609861b733851a12841
Muhammad Farooq Iqbal <mfarooqiqbal143@gmail.com>
2024-04-04 15:57:26 +0500

    WIP web url

--- lib/utills/app_consts.dart
+++ lib/utills/app_consts.dart
@@ -10,5 +10,5 @@ class AppConsts {
 
   ///
   static String webUrl = 'http://66.42.49.235:3031/stripe/stripe_webview.html';
-  static String returnUrl = '$webUrl/dashboard';
+  static String returnUrl = 'http://66.42.49.235:3031';
 }
